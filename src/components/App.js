c8d938bb46c5c9b1550b75b14b20479c2abee3fd
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-07-23 19:10:34 +1000

    add fetch method to retrieve json data and pass it list-appointments component as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 
 //components
@@ -6,22 +6,47 @@ import AddAppointments from './AddAppointments'
 import SearchAppointments from './SearchAppointments'
 import ListAppointments from './ListAppointments'
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends Component {
+
+  state = {
+    appointments: [],
+    isLoading: false,
+  }
+
+  async componentDidMount() {
+
+    this.setState({ isLoading: true })
+
+    try {
+
+      const response = await fetch('./data.json')
+      const data = response.json()
+      this.setState({ appointments: data, isLoading: false })
+
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+
+  render() {
+    const { appointments } = this.state
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={appointments}/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    )
+  }
 }
 
 export default App;
